59e1285b29fa90a9a5dc55719ddbef8182a7951c
hoefkensj <hoefkens.j@gmail.com>
2023-05-27 11:53:37 +0200

    ${files:x}

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
+
+Import PyQt, any version and use it and tits modules versionless
+Currently Posix (Linux/MacOs) only srry
+(as i dont have a working windows env atm)
+tested on Linux(Gentoo) with python  3.9,3.10,3.11 and PyQt 5,6
+untested on MacOs but should work
+
 example: 
 ```py
 from sys import argv, exit
-from Qt import PyQt
+from PyQt import PyQt
 
 QtApp = PyQt.QtWidgets.QApplication(argv)
 wgt = PyQt.QtWidgets.QWidget()
--- examples/minimal_PyQt.py
+++ examples/minimal_PyQt.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 # ##############################################################################
 # # PROJ: PyQt                           AUTHORS:         Hoefkens.j@gmail.com #
-# # FILE: test_Qt.py                                                           #
+# # FILE: minimal_PyQt.py                                                      #
 # # REPO: hoefkensj/PyQt.git                                                   #
 # # HOST: github.com                                                           #
 # # VERSION: 0.1.2                                                             #
 # # UPDATED:  20230512                                                         #
 # ##############################################################################
 #
-import unittest
-from src import PyQt
+from sys import argv, exit
+from PyQt import PyQt
 
-
-class Tests(unittest.TestCase):
-	def test_PyQt(self):
-		Pyqt_Core=PyQt.PyQt.QtCore
-		self.assertIsNotNone(PyQt.PyQt.QtCore)
-		return self
-
-
-if __name__ == "__main__":
-	unittest.main()
+QtApp = PyQt.QtWidgets.QApplication(argv)
+wgt = PyQt.QtWidgets.QWidget()
+layout = PyQt.QtWidgets.QVBoxLayout(wgt)
+lbl = PyQt.QtWidgets.QLabel()
+lbl.setText(f'Using: {PyQt.__name__}')
+layout.addWidget(lbl)
+wgt.show()
+exit(QtApp.exec())
