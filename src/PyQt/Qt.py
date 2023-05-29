5a0380d4f97c337c9fce8586dbdfb83ac727762b
hoefkensj <hoefkens.j@gmail.com>
2023-05-29 17:54:32 +0200

    ${files:x}

--- src/PyQt/Qt.py
+++ src/PyQt/Qt.py
@@ -8,69 +8,57 @@
 # # UPDATED:  20230512                                                         #
 # ##############################################################################
 #
-
-from pathlib import Path,os
+import re
+from os import name
+from re import compile,match
+from pathlib import Path
 from inspect import getmodulename
 from importlib import import_module
 from pkgutil import iter_modules
 
-def OSParameters(OS):
-	P={}
-	P['posix']={}
-	P['posix']['dirs']= ['usr',]
-	P['posix']['arch']= ['','32','64']
-	P['posix']['path']= ['/{DIR}/lib{ARCH}/']
+def detect(**versions):
+	Modules=['PyQt','PySide']
+	versions={mod : [] for mod in Modules}
+	reStr='(?P<MOD>{MOD})(?P<V>\d+)'
+	REXQT=compile(reStr.format(MOD=Modules[0]))
+	REXSIDE=compile(reStr.format(MOD=Modules[1]))
+	for installed in [*iter_modules()]:
+		if (QT:=REXQT.match(installed.name)) or (SIDE:=REXSIDE.match(installed.name)):
+			if QT:
+				module=QT.groupdict()['MOD']
+				version= QT.groupdict()['V']
+			else:
+				module=SIDE.groupdict()['MOD']
+				version= SIDE.groupdict()['V']
+			versions[module]+=[version]
+	return versions
 
-	if P.get(OS):
-		for dir in	P[OS]['dirs']:
-			for arch in 	P[OS]['arch']:
-				P[OS]['path']+=[
-					P[OS]['path'][0].format(DIR=dir,ARCH=arch)
-				]
-	return P.get(OS)
-
-
-def detectQt(P):
-	def dQt():
-		nonlocal d
-		for path in P.get('path')[1:]:
-			for qt in Path(path).glob("qt[0-9]*"):
-				if qt.name.__str__()[2:].isnumeric():
-					d['Qt'] += [qt.name.__str__()[2:]]
-
-	def dPyQt():
-		nonlocal d
-		for module in iter_modules():
-			if module.name.casefold().startswith("pyqt"):
-					candidate = module.name.removeprefix("PyQt")
-					if candidate.isnumeric():
-							d['PyQt'] += [int(candidate)]
-
-	d={'Qt': [], 'PyQt': [] }
-	dQt()
-	dPyQt()
-	return d
-
-def ImportPyQt(versions):
-	vPyQt=versions['PyQt']
-	def importPyQt(version=None, modules=None):
-		latest = vPyQt[-1]
-		version = version if version in vPyQt else latest
-		PyQt = import_module(f"PyQt{version}")
-		return PyQt
-	return importPyQt
+def ImportMod(*version,mod='PyQt'):
+	global Versions
+	if not version:
+		if not Versions.get(mod):
+			result=None
+		else:
+			result=import_module(f"{mod}{Versions.get(mod)[-1]}")
+	else:
+		if not Versions.get(mod) or version not in Versions.get(mod) :
+			result=None
+		else :
+			result=import_module(f"{mod}{version}")
+	return result
 
 
-def importPyQtModules(PyQt):
+
+def importSubMod(mod):
 	for filename in Path(PyQt.__file__).parent.glob(f"Qt*.*"):
 		name = getmodulename(filename.name)
 		if name:
 			PyQt.__setattr__(name, import_module(f"{PyQt.__name__}.{name}"))
 	return PyQt
 
-OS=os.name
-OSParams=OSParameters(OS)
-Versions=detectQt(OSParams)
-ImportPyQt=ImportPyQt(Versions)
-PyQt = ImportPyQt()
-PyQt = importPyQtModules(PyQt)
+Versions=detect()
+PyQt=ImportMod(mod='PyQt')
+PyQt = importSubMod(PyQt)
+for attr in  dir(PyQt):
+	if not attr.startswith('__'):
+		print(attr,getattr(PyQt,attr) )
